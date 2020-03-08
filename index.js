9bd188a054544875c174ba53963bbfa2cb928e78
linhbui98 <linhbuiquang2321998@gmail.com>
2020-03-08 07:23:52 +0700

    mvc

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
 
-const adapter = new FileSync('db.json')
-const db = low(adapter)
-
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] })
-  .write()
+const userRoute = require('./routes/user.route.js')
 
 const app = express()
 const port = 3000
@@ -23,29 +16,7 @@ app.get('/', (req, res) => res.render('index', {
     name: 'Anh Linh đẹp trai!'
 }))
 
-app.get('/users', (req, res) => res.render('users/index', {
-    users: db.get('users').value()
-}))
-
-app.get('/users/search', (req, res) => {
-    let q = req.query.q
-    let matchedUsers = db.get('users').value().filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
-
-    res.render('users/index', {
-        users: matchedUsers
-    })
-})
-
-app.get('/users/create', (req, res) => {
-    res.render('users/create')
-})
-
-app.post('/users/create', (req, res) => {
-    db.get('users')
-    .push(req.body)
-    .write()
-    res.redirect("/users")
-})
+app.use('/users', userRoute)
 
 app.listen(port, () => console.log(`Example app listening on port: ${port}`))
 
